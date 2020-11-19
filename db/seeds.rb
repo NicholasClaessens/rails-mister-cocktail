09cb3a5bd72a34f425bf2c45d73295b4efdd206c
Nicholas Claessens <nicholas04claessens@yahoo.com>
2020-11-19 15:01:23 +0100

    Bar a cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cocktail.create(name: "Cuba Libre")
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Margarita")
